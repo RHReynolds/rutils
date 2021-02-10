4590f3b69883cbb5b550f5b1fd0e2d1a3f1a1026
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2021-02-10 13:33:04 +0000

    Tidy documentation and add example

--- R/liftover_coord.R
+++ R/liftover_coord.R
@@ -1,13 +1,60 @@
 #' Liftover genomic coordinates from one genome build to another.
 #'
-#' @param df Dataframe with genomic coordinates. Must include columns, \code{CHR} and
-#'   \code{BP}, with chromosome and base pair, respectively. If additional
-#'   columns included these will be preserved.
-#' @param path_to_chain Path to UCSC chain file for transformation. This can
-#'   either be from hg19 to hg38 or hg38 to hg19.
+#' @description This function allows users to lift genomic co-ordinates from one
+#'   genome build to another. It uses an over.chain liftOver conversion file.
+#'   Conversion files can be downloaded from
+#'   [UCSC](http://hgdownload.soe.ucsc.edu/downloads.html#liftover).
+#'
+#' @param df a `data.frame` object with genomic coordinates. Must include the
+#'   following columns: 
+#'   \itemize{ 
+#'   \item `CHR` - `character()` vector, with "chr"
+#'   attached to the chromosome e.g. `chr1` 
+#'   \item `BP` - base pair, as a
+#'   `numeric()` vector } 
+#'   Any additional columns included will be preserved.
+#' @param path_to_chain a `character()` vector, with the file path to the UCSC
+#'   chain file for transformation.
+#'
 #'
 #' @return Dataframe with coordinates in different genome build.
 #' @export
+#' 
+#' @examples 
+#' file_path <- 
+#'   system.file(
+#'     "testdata", 
+#'     "liftover_test_data.txt", 
+#'     package = "rutils", 
+#'     mustWork = TRUE
+#'   )
+#' 
+#' hg19_path <- 
+#'   system.file(
+#'     "testdata", 
+#'     "hg19ToHg38.over.chain", 
+#'     package = "rutils", 
+#'     mustWork = TRUE
+#'   )
+#' 
+#' genomic_df <- 
+#'   readr::read_delim(file_path,
+#'                     delim = " ",
+#'                     col_types =
+#'                       readr::cols(
+#'                         SNP = readr::col_character(),
+#'                         CHR = readr::col_factor(),
+#'                         BP_hg19 = readr::col_integer(),
+#'                         BP_hg38 = readr::col_integer()
+#'                       ))
+#' 
+#' liftover_coord(df = 
+#'                  genomic_df %>% 
+#'                  dplyr::select(SNP, 
+#'                                CHR, 
+#'                                BP = BP_hg19), 
+#'                path_to_chain = hg19_path
+#' )
 
 liftover_coord <- function(df, path_to_chain){
   
